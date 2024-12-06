a7401b2e32f6f3cd8b077deced5a9af6ade561d8
Ethan Muller <eman3001@yahoo.com>
2024-12-05 20:47:56 -0500

    Add Right Paddle

---
+++ PaddlR.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class PaddlR : AnimationPlayer
+{
+    [Export] AnimationPlayer animPlayer;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+        if (Input.IsActionJustPressed("paddle_right"))
+        {
+            animPlayer.Play("paddleRight");
+        }
+    }
+}
