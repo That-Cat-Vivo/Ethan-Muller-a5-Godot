f82cf8be060b2b6d81fb8cf7e88ff68200771475
Ethan Muller <eman3001@yahoo.com>
2024-12-05 19:39:52 -0500

    Add prefabs for Paddles, Bumpers and Walls. Add Animation and AnimationPlayer for Paddle

---
+++ Paddl.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class Paddl : AnimationPlayer
+{
+	[Export] AnimationPlayer animPlayer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("paddle_left"))
+		{
+			animPlayer.Play("new_animation");
+		}
+	}
+
+  
+}
