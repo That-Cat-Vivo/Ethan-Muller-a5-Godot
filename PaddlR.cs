563c31d2a70cfc6107998a287cc8abcd03d12ace
Ethan Muller <eman3001@yahoo.com>
2024-12-10 12:30:59 -0500

    Add Bumper Wedge Prefab Updated Game Layout

--- PaddlR.cs
+++ PaddlR.cs
@@ -4,6 +4,7 @@ using System;
 public partial class PaddlR : AnimationPlayer
 {
     [Export] AnimationPlayer animPlayer;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
